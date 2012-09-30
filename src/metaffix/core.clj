9edea387848ac72dce63cfd356a11fa125ba6720
Aaron Miller <apage43@ninjawhale.com>
2012-09-29 23:31:24 -0700

    Use spymemcached CAS mechanism
    plus a bit of cleanup

--- src/metaffix/core.clj
+++ src/metaffix/core.clj
@@ -1,42 +1,38 @@
 (ns metaffix.core
   (:use [clojure.tools.cli :only [cli]])
+  (:import [net.spy.memcached CachedData CASMutator CASMutation]
+           [net.spy.memcached.transcoders Transcoder])
   (:require [metaffix.view :as view]
             [metaffix.tika :as tika]
             [clojure.string :as string]
             [clojure.walk :as walk]
             [cheshire.core :as json]
-            [clojurewerkz.spyglass.couchbase :as cbcli]
             [clj-http.client :as http]
+            [clojurewerkz.spyglass.couchbase :as cbcli]
             [clojurewerkz.spyglass.client :as cb])
   (:gen-class))
 
-; Spyglass's gets is busted for non-existing keys
-(defn gets [conn k]
-  (let [retr (.gets conn k)]
-    (if retr
-      {:value (.getValue retr) :cas (.getCas retr)})))
+(def json-transcoder
+  (proxy [Transcoder] []
+    (asyncDecode [_] false)
+    (decode [bs] (json/parse-string (String. (.getData bs)) true))
+    (encode [o] (CachedData. 0 (.getBytes (json/encode o)) CachedData/MAX_SIZE))
+    (getMaxSize [] CachedData/MAX_SIZE)))
 
 (defn cbitem
-  "Fetch JSON item with `key` from `conn`, and return parsed value with CAS id on metadata as :cas"
+  "Fetch JSON item with `key` from `conn`"
   [conn k]
-  (let [item (gets conn k)]
-    (if item (with-meta (json/parse-string (:value item) true) {:cas (:cas item)}))))
+  (cb/get conn k json-transcoder))
 
 (defn cb-update
   "Atomically update a JSON item with the result of applying f to it."
-  ([conn k f]
-   (cb-update conn k f 1000))
-  ([conn k f maxretries]
-   (try
-     (loop [retries maxretries]
-      (if (pos? retries)
-        (let [item (cbitem conn k)]
-          (if (nil? item) :item_error
-            (if (not= :ok
-                      (cb/cas conn k (:cas (meta item)) (json/encode (f item))))
-              (recur (dec retries))
-              :ok))) :too_many_retries))
-     (catch Exception e :item_error))))
+  [conn k f]
+   (let [mutation
+         (proxy [CASMutation] []
+           (getNewValue [current]
+             (f current)))
+         mutator (CASMutator. conn json-transcoder)]
+     (.cas mutator k nil 0 mutation)))
 
 (defn capi-bases
   "Get CAPI base URL for a Couchbase cluster"
@@ -47,19 +43,29 @@
        (mapv :couchApiBase)))
 
 (defn cbfs-files [capis bucket]
-  (->> (view/view-seq (str (rand-nth capis) bucket "/_design/cbfs/_view/file_browse") nil {} {:include_docs true})
+  (->> (view/view-seq
+         (str (rand-nth capis) bucket "/_design/cbfs/_view/file_browse")
+         nil {} ; All the files
+         {:include_docs true})
        (map (comp (juxt (comp :id :meta) :json) :doc))))
 
-(defn fetch-blob
-  "Fetch a blob by OID. Will try each node marked as having the blob until one succeeds, else returns nil. On success returns a stream."
-  [cbconn oid]
+(defn open-blob
+  [cbconn oid candidate-nodes]
+  (first (drop-while nil? (map (fn [nodename]
+                                 (let [nodedata (cbitem cbconn (str "/" (name nodename)))
+                                       bloburl (str "http://" (:addr nodedata) (:bindaddr nodedata) "/.cbfs/blob/" oid)]
+                                   (try (:body (http/get bloburl {:as :stream})) (catch Exception e nil))))
+                               candidate-nodes))))
+(defn process-blob
+  "Fetch a blob by OID. Will try each node marked as having the blob until one succeeds,
+   else returns nil. On success returns the value of calling f on the resulting stream."
+  [cbconn oid f]
   (let [blobdata (cbitem cbconn (str "/" oid))
-        candidate-nodes (keys (:nodes blobdata))]
-    (first (drop-while nil? (map (fn [nodename]
-                                   (let [nodedata (cbitem cbconn (str "/" (name nodename)))
-                                         bloburl (str "http://" (:addr nodedata) (:bindaddr nodedata) "/.cbfs/blob/" oid)]
-                                     (try (:body (http/get bloburl {:as :stream})) (catch Exception e nil))))
-                                 candidate-nodes)))))
+        candidate-nodes (keys (:nodes blobdata))
+        blobstream (open-blob cbconn oid candidate-nodes)]
+    (when blobstream
+      (with-open [bs blobstream]
+        (f bs)))))
 
 (defn metaffix [{cburl :couchbase cbbucket :bucket cbpass :password
                  maxsize :max-size minsize :min-size}]
@@ -75,15 +81,15 @@
                                   (or (nil? (:metaffix userdata))
                                       (not= (get-in userdata [:metaffix :oid]) oid)))) (cbfs-files capis cbbucket))]
     (doseq [[id {:keys [oid]}] eligible-files]
-      (let [item-stream (fetch-blob cbconn oid)]
-        (if item-stream
-          (let [metadata (assoc (tika/parse item-stream) :oid oid)]
-            (if (= :ok (cb-update cbconn id
-                                        #(update-in % [:userdata] assoc :metaffix metadata)))
-              (println "Affixed metadata to file: " id)
-              (println "WARN: Couldn't update file " id))
-            (.close item-stream))
-          (println "Could not fetch blob: " oid)))))
+      (let [metadata (process-blob cbconn oid tika/parse)]
+        (if metadata
+          (if (try (cb-update cbconn id #(update-in % [:userdata]
+                                               assoc :metaffix (assoc metadata :oid oid)))
+                (catch Exception e
+                  (println "Exception updating metadata on file" e)))
+            (println "Metadata affixed to file" id)
+            (println "Error affixing metadata to file" id))
+          (println "Error processing file" id)))))
   (println "Done!"))
 
 (def num-suffixes
@@ -106,11 +112,12 @@
                     ["-c" "--couchbase" "URL of Couchbase cluster" :default "http://localhost:8091/"]
                     ["-b" "--bucket" "CBFS bucket name on Couchbase" :default "cbfs"]
                     ["-p" "--password" "Password for the Couchbase bucket" :default ""]
-                    ["-m" "--max-size" "Don't attempt to tag files larger than this size. (Can use K, M, G suffix)" :default 10485760 :parse-fn dehumanize]
-                    ["-n" "--min-size" "Don't attempt to tag files smaller than this size. (Can use K, M, G suffix)" :default 0 :parse-fn dehumanize]
+                    ["-m" "--max-size" "Don't attempt to tag files larger than this size" :default 10485760 :parse-fn dehumanize]
+                    ["-n" "--min-size" "Don't attempt to tag files smaller than this size" :default 0 :parse-fn dehumanize]
                     ["-h" "--help" "Show help" :default false :flag true])]
     (when (:help config)
       (println usage)
+      (println "Sizes can take numbers suffixed with K, M, G.")
       (System/exit 0))
     (println "Config: " (pr-str config))
     (metaffix config)))
